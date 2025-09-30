adfaf9a46d23c4bbb95f1877860f707b680b3e98
Rajorshi Das <das2107060@stud.kuet.ac.bd>
2025-09-30 23:51:31 +0600

    addeding layouts

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -3,18 +3,14 @@ import { Head } from '@inertiajs/react';
 import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
+import { Children } from 'react';
 
 export default function Edit({ mustVerifyEmail, status }) {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Profile
-                </h2>
-            }
-        >
-            <Head title="Profile" />
 
+
+   <>
+       <Head title="Profile" />
             <div className="py-12">
                 <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
@@ -34,6 +30,16 @@ export default function Edit({ mustVerifyEmail, status }) {
                     </div>
                 </div>
             </div>
+</>
+    );
+}
+Edit.layout = (page) => {
+    return (
+        <AuthenticatedLayout
+            user={page.props.auth.user}
+            header={<h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Profile</h2>}
+            children={page}>
+
         </AuthenticatedLayout>
     );
 }
